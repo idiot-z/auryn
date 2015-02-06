c6f351e8121273a0233c81045022958dcdacf981
Lorric Ziegler <lorric.ziegler@gmail.com>
2015-02-06 17:50:34 +0100

    indentation & Triplet accepts no homeostasis

--- src/TripletConnection.cpp
+++ src/TripletConnection.cpp
@@ -1,76 +1,79 @@
-/* 
-* Copyright 2014 Friedemann Zenke
-*
-* This file is part of Auryn, a simulation package for plastic
-* spiking neural networks.
-* 
-* Auryn is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-* 
-* Auryn is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-* 
-* You should have received a copy of the GNU General Public License
-* along with Auryn.  If not, see <http://www.gnu.org/licenses/>.
-*
-* If you are using Auryn or parts of it for your work please cite:
-* Zenke, F. and Gerstner, W., 2014. Limits to high-speed simulations 
-* of spiking neural networks using general-purpose computers. 
-* Front Neuroinform 8, 76. doi: 10.3389/fninf.2014.00076
-*/
+/*
+ * Copyright 2014 Friedemann Zenke
+ *
+ * This file is part of Auryn, a simulation package for plastic
+ * spiking neural networks.
+ *
+ * Auryn is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Auryn is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Auryn.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * If you are using Auryn or parts of it for your work please cite:
+ * Zenke, F. and Gerstner, W., 2014. Limits to high-speed simulations
+ * of spiking neural networks using general-purpose computers.
+ * Front Neuroinform 8, 76. doi: 10.3389/fninf.2014.00076
+ */
 
 #include "TripletConnection.h"
 
 void TripletConnection::init(AurynFloat tau_hom, AurynFloat eta, AurynFloat kappa, AurynFloat maxweight)
 {
-	if ( dst->get_post_size() == 0 ) return; // avoids to run this code on silent nodes with zero post neurons.
+        if ( dst->get_post_size() == 0 ) return; // avoids to run this code on silent nodes with zero post neurons.
 
-	/* Initialization of plasticity parameters. */
-	A3_plus = 6.5e-3;
-	A3_plus *= eta;
+        /* Initialization of plasticity parameters. */
+        A3_plus = 6.5e-3;
+        A3_plus *= eta;
 
-	tau_plus  = 16.8e-3;
-	tau_minus = 33.7e-3;
-	tau_long  = 114e-3;
+        tau_plus  = 16.8e-3;
+        tau_minus = 33.7e-3;
+        tau_long  = 114e-3;
 
-	tau_homeostatic = tau_hom;
+        tau_homeostatic = tau_hom;
 
-	/* Initialization of presynaptic traces */
-	tr_pre = src->get_pre_trace(tau_plus);
+        /* Initialization of presynaptic traces */
+        tr_pre = src->get_pre_trace(tau_plus);
 
-	/* Initialization of postsynaptic traces */
-	tr_post = dst->get_post_trace(tau_minus);
-	tr_post2 = dst->get_post_trace(tau_long);
-	tr_post_hom = dst->get_post_trace(tau_hom);
+        /* Initialization of postsynaptic traces */
+        tr_post = dst->get_post_trace(tau_minus);
+        tr_post2 = dst->get_post_trace(tau_long);
+	if (tau_hom > 0) {
+		tr_post_hom = dst->get_post_trace(tau_hom);
+		hom_fudge = A3_plus*tau_plus*tau_long/tau_minus/kappa/tau_hom/tau_hom;
+	} else {
+		hom_fudge = A3_plus*tau_plus*tau_long/tau_minus;
+	}
 
-	hom_fudge = A3_plus*tau_plus*tau_long/(tau_minus)/kappa/tau_hom/tau_hom;
+        /* Set min/max weight values. */
+        set_min_weight(0.0);
+        set_max_weight(maxweight);
 
-	/* Set min/max weight values. */
-	set_min_weight(0.0);
-	set_max_weight(maxweight);
-
-	stdp_active = true;
+        stdp_active = true;
 
 }
 
-void TripletConnection::init_shortcuts() 
+void TripletConnection::init_shortcuts()
 {
-	if ( dst->get_post_size() == 0 ) return; // if there are no target neurons on this rank
+        if ( dst->get_post_size() == 0 ) return; // if there are no target neurons on this rank
 
-	fwd_ind = w->get_row_begin(0); 
-	fwd_data = w->get_data_begin();
+        fwd_ind = w->get_row_begin(0);
+        fwd_data = w->get_data_begin();
 
-	bkw_ind = bkw->get_row_begin(0); 
-	bkw_data = bkw->get_data_begin();
+        bkw_ind = bkw->get_row_begin(0);
+        bkw_data = bkw->get_data_begin();
 }
 
 void TripletConnection::finalize() {
-	DuplexConnection::finalize();
-	init_shortcuts();
+        DuplexConnection::finalize();
+        init_shortcuts();
 }
 
 void TripletConnection::free()
@@ -81,147 +84,148 @@ TripletConnection::TripletConnection(SpikingGroup * source, NeuronGroup * destin
 {
 }
 
-TripletConnection::TripletConnection(SpikingGroup * source, NeuronGroup * destination, 
-		const char * filename, 
-		AurynFloat tau_hom, 
-		AurynFloat eta, 
-		AurynFloat kappa, AurynFloat maxweight , 
-		TransmitterType transmitter) 
-: DuplexConnection(source, 
-		destination, 
-		filename, 
-		transmitter)
+TripletConnection::TripletConnection(SpikingGroup * source, NeuronGroup * destination,
+                                     const char * filename,
+                                     AurynFloat tau_hom,
+                                     AurynFloat eta,
+                                     AurynFloat kappa, AurynFloat maxweight ,
+                                     TransmitterType transmitter)
+        : DuplexConnection(source,
+                           destination,
+                           filename,
+                           transmitter)
 {
-	init(tau_hom, eta, kappa, maxweight);
-	init_shortcuts();
+        init(tau_hom, eta, kappa, maxweight);
+        init_shortcuts();
 }
 
-TripletConnection::TripletConnection(SpikingGroup * source, NeuronGroup * destination, 
-		AurynWeight weight, AurynFloat sparseness, 
-		AurynFloat tau_hom, 
-		AurynFloat eta, 
-		AurynFloat kappa, AurynFloat maxweight , 
-		TransmitterType transmitter,
-		string name) 
-: DuplexConnection(source, 
-		destination, 
-		weight, 
-		sparseness, 
-		transmitter, 
-		name)
+TripletConnection::TripletConnection(SpikingGroup * source, NeuronGroup * destination,
+                                     AurynWeight weight, AurynFloat sparseness,
+                                     AurynFloat tau_hom,
+                                     AurynFloat eta,
+                                     AurynFloat kappa, AurynFloat maxweight ,
+                                     TransmitterType transmitter,
+                                     string name)
+        : DuplexConnection(source,
+                           destination,
+                           weight,
+                           sparseness,
+                           transmitter,
+                           name)
 {
-	init(tau_hom, eta, kappa, maxweight);
-	if ( name.empty() ) 
-		set_name("TripletConnection");
-	init_shortcuts();
+        init(tau_hom, eta, kappa, maxweight);
+        if ( name.empty() )
+                set_name("TripletConnection");
+        init_shortcuts();
 }
 
 TripletConnection::~TripletConnection()
 {
-	if ( dst->get_post_size() > 0 ) 
-		free();
+        if ( dst->get_post_size() > 0 )
+                free();
 }
 
 void TripletConnection::set_hom_trace(AurynFloat freq)
 {
-	if ( dst->get_post_size() > 0 ) 
-		tr_post_hom->set_all(freq*tr_post_hom->get_tau());
+        if ( dst->get_post_size() > 0 && tau_homeostatic > 0)
+                tr_post_hom->set_all(freq*tr_post_hom->get_tau());
 }
 
 
 AurynWeight TripletConnection::get_hom(NeuronID i)
 {
-	return pow(tr_post_hom->get(i),2);
+	if (tau_homeostatic > 0)
+		return pow(tr_post_hom->get(i),2);
+	else
+		return 1;
 }
 
 
-/*! This function implements what happens to synapes transmitting a 
+/*! This function implements what happens to synapes transmitting a
  *  spike to neuron 'post'. */
 void TripletConnection::dw_pre(NeuronID post, AurynWeight * weight)
 {
-	// translate post id to local id on rank: translated_spike
-	NeuronID translated_spike = dst->global2rank(post); 
-	AurynDouble dw = -hom_fudge*(tr_post->get(translated_spike)*get_hom(translated_spike));
-	*weight += dw;
-	// clips too small weights
-	if ( *weight < get_min_weight() ) 
-		*weight = get_min_weight();
+        // translate post id to local id on rank: translated_spike
+        NeuronID translated_spike = dst->global2rank(post);
+        AurynDouble dw = -hom_fudge*(tr_post->get(translated_spike)*get_hom(translated_spike));
+        *weight += dw;
+        // clips too small weights
+        if ( *weight < get_min_weight() )
+                *weight = get_min_weight();
 }
 
-/*! This function implements what happens to synapes experiencing a 
+/*! This function implements what happens to synapes experiencing a
  *  backpropagating action potential from neuron 'pre'. */
 void TripletConnection::dw_post(NeuronID pre, NeuronID post, AurynWeight * weight)
 {
-	// at this point post was already translated to a local id in 
-	// the propagate_backward function below.
-	AurynDouble dw = A3_plus*tr_pre->get(pre)*tr_post2->get(post);
-	*weight += dw;
-	// clips too large weights
-	if (*weight>get_max_weight()) *weight=get_max_weight();
+        // at this point post was already translated to a local id in
+        // the propagate_backward function below.
+        AurynDouble dw = A3_plus*tr_pre->get(pre)*tr_post2->get(post);
+        *weight += dw;
+        // clips too large weights
+        if (*weight>get_max_weight()) *weight=get_max_weight();
 }
 
 
 void TripletConnection::propagate_forward()
 {
-	// loop over all spikes
-	for (SpikeContainer::const_iterator spike = src->get_spikes()->begin() ; // spike = pre_spike
-			spike != src->get_spikes()->end() ; ++spike ) {
-		// loop over all postsynaptic partners
-		for (const NeuronID * c = w->get_row_begin(*spike) ; 
-				c != w->get_row_end(*spike) ; 
-				++c ) { // c = post index
+        // loop over all spikes
+        for (SpikeContainer::const_iterator spike = src->get_spikes()->begin() ; // spike = pre_spike
+             spike != src->get_spikes()->end() ; ++spike ) {
+                // loop over all postsynaptic partners
+                for (const NeuronID * c = w->get_row_begin(*spike) ;
+                     c != w->get_row_end(*spike) ;
+                     ++c ) { // c = post index
 
-			// transmit signal to target at postsynaptic neuron
-			AurynWeight * weight = w->get_data_ptr(c); 
-			transmit( *c , *weight );
+                        // transmit signal to target at postsynaptic neuron
+                        AurynWeight * weight = w->get_data_ptr(c);
+                        transmit( *c , *weight );
 
-			// handle plasticity
-			if ( stdp_active ) {
-				// performs weight update
-				dw_pre(*c,weight);
+                        // handle plasticity
+                        if ( stdp_active ) {
+                                // performs weight update
+                                dw_pre(*c,weight);
 
-			}
-		}
-	}
+                        }
+                }
+        }
 }
 
 void TripletConnection::propagate_backward()
 {
-	if (stdp_active) { 
-		SpikeContainer::const_iterator spikes_end = dst->get_spikes_immediate()->end();
-		// loop over all spikes
-		for (SpikeContainer::const_iterator spike = dst->get_spikes_immediate()->begin() ; // spike = post_spike
-				spike != spikes_end ; 
-				++spike ) {
-			// Since we need the local id of the postsynaptic neuron that spiked 
-			// multiple times, we translate it here:
-			NeuronID translated_spike = dst->global2rank(*spike); 
+        if (stdp_active) {
+                SpikeContainer::const_iterator spikes_end = dst->get_spikes_immediate()->end();
+                // loop over all spikes
+                for (SpikeContainer::const_iterator spike = dst->get_spikes_immediate()->begin() ; // spike = post_spike
+                     spike != spikes_end ;
+                     ++spike ) {
+                        // Since we need the local id of the postsynaptic neuron that spiked
+                        // multiple times, we translate it here:
+                        NeuronID translated_spike = dst->global2rank(*spike);
 
-			// loop over all presynaptic partners
-			for (const NeuronID * c = bkw->get_row_begin(*spike) ; c != bkw->get_row_end(*spike) ; ++c ) {
+                        // loop over all presynaptic partners
+                        for (const NeuronID * c = bkw->get_row_begin(*spike) ; c != bkw->get_row_end(*spike) ; ++c ) {
 
-				#ifdef CODE_ACTIVATE_PREFETCHING_INTRINSICS
-				// prefetches next memory cells to reduce number of last-level cache misses
-				_mm_prefetch((const char *)bkw_data[c-bkw_ind+2],  _MM_HINT_NTA);
-				#endif
+#ifdef CODE_ACTIVATE_PREFETCHING_INTRINSICS
+                                // prefetches next memory cells to reduce number of last-level cache misses
+                                _mm_prefetch((const char *)bkw_data[c-bkw_ind+2],  _MM_HINT_NTA);
+#endif
 
-				// computes plasticity update
-				AurynWeight * weight = bkw->get_data(c); 
-				dw_post(*c,translated_spike,weight);
+                                // computes plasticity update
+                                AurynWeight * weight = bkw->get_data(c);
+                                dw_post(*c,translated_spike,weight);
 
-			}
-		}
-	}
+                        }
+                }
+        }
 }
 
 void TripletConnection::propagate()
 {
-	propagate_forward();
-	propagate_backward();
+        propagate_forward();
+        propagate_backward();
 }
 
 void TripletConnection::evolve()
 {
 }
-
-
